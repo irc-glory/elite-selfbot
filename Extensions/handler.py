42bcfcf83dc9923a7a4af9eea8bf4623d5fdbfeb
irc-glory <discreetdorian@gmail.com>
2024-10-02 18:05:05 +0300

    PUSH

---
+++ Extensions/handler.py
@@ -0,0 +1,2 @@
+# add functions that can be used by the extensions 
+# before running extensions, make sure they are verified on the verified extensions list! if not, warn the user and ask if they want to proceed.
---
+++ main.py
@@ -0,0 +1,9 @@
+import os, webview
+from Extensions.handler import *
+
+if not os.path.exists('.assets/config.json'):
+    window = webview.create_window('Setup', f'file://{os.path.join(os.path.dirname(__file__), '.assets', 'setup.html')}')
+    webview.start()
+else:
+    # run the actual client
+    pass
