37087b1ce7b998ae57937400e0b5f48a35cdafa7
irc-glory <discreetdorian@gmail.com>
2024-10-02 18:07:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, webview
 from Extensions.handler import *
 
 if not os.path.exists('.assets/config.json'):
-    window = webview.create_window('Setup', f'file://{os.path.join(os.path.dirname(__file__), '.assets', 'setup.html')}')
+    window = webview.create_window('Setup', f'file://{os.path.join(os.path.dirname(__file__), '#assets', 'setup.html')}')
     webview.start()
 else:
     # run the actual client
